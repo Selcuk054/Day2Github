dd107af8e021b6dff7b0cf61a0a3ac51bb4cd3b3
Selcuk Ufuk <selcukkrds09@gmail.com>
2022-08-14 12:47:56 +0200

    3.C

--- src/Day1Git2.java
+++ src/Day1Git2.java
@@ -2,5 +2,6 @@ public class Day1Git2 {
     public static void main(String[] args) {
         System.out.println("Hello wereld, Hoe is het 1");
         System.out.println("Hello wereld, Hoe is het 2");
+        System.out.println("Hello wereld, Hoe is het 3");
     }
 }
