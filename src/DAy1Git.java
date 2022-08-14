1d69e97baf5043a5d59d1dfb16cbc38b3b1a60fa
Selcuk Ufuk <selcukkrds09@gmail.com>
2022-08-14 12:54:55 +0200

    new

--- src/DAy1Git.java
+++ src/DAy1Git.java
@@ -1,5 +1,8 @@
 public class DAy1Git {
     public static void main(String[] args) {
         System.out.println("Hello Git 1. Deneme");
+        System.out.println("Hello Git 1. Deneme");
+        System.out.println("Hello Git 1. Deneme");
+        System.out.println("Hello Git 1. Deneme");
     }
 }
